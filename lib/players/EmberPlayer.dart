73e37edcf0177b54be6867cdd5b392ee09e56755
alumno <dam.sonia.fernandez@gmail.com>
2023-01-08 13:49:09 +0100

    creacicon clase EmberPlayer

---
+++ lib/players/EmberPlayer.dart
@@ -0,0 +1,21 @@
+import 'package:flame/components.dart';
+import 'package:ugh/game/UghGame.dart';
+
+class EmberPlayer extends SpriteAnimationComponent
+    with HasGameRef<UghGame> {
+  EmberPlayer({
+    required super.position,
+  }) : super(size: Vector2.all(64), anchor: Anchor.center);
+
+  @override
+  Future<void> onLoad() async {
+    animation = SpriteAnimation.fromFrameData(
+      game.images.fromCache('ember.png'),
+      SpriteAnimationData.sequenced(
+        amount: 4,
+        textureSize: Vector2.all(16),
+        stepTime: 0.12,
+      ),
+    );
+  }
+}
