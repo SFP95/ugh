8bed59a5225e26b3286593172215ea4a9a50151f
alumno <dam.sonia.fernandez@gmail.com>
2023-01-08 12:29:15 +0100

    cambio de main + creacion assets y subida de imagenes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  final game = FlameGame();
+  runApp(GameWidget(game: game));
 }
