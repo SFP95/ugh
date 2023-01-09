20f587900fe2792acc9c3e35369ff661edcbc3ce
alumno <dam.sonia.fernandez@gmail.com>
2023-01-09 21:05:02 +0100

    ajustes, ya se ven enemigos y objetos estrella

--- lib/game/UghGame.dart
+++ lib/game/UghGame.dart
@@ -27,23 +27,27 @@ class UghGame extends FlameGame with HasKeyboardHandlerComponents,HasCollisionDe
       'star.png',
       'water_enemy.png',
     ]);
-
+    //carcago de mapa
     TiledComponent mapComponent = await TiledComponent.load(
         "mapa.tmx", Vector2(32, 32));
     add(mapComponent);
+    //cargado de jugados ember
+    EmberPlayer emberPlayer= EmberPlayer(position: Vector2(300,300));
+    add(emberPlayer);
+
+    //cargado de gotas enemigo sy objetos estrella
     ObjectGroup? estrellas = mapComponent.tileMap.getLayer<ObjectGroup>(
         "estrellas");
     ObjectGroup? gotas = mapComponent.tileMap.getLayer<ObjectGroup>("gotas");
 
+    //fondo de pantalla
     @override
     Color backgroundColor() {
       return const Color.fromARGB(255, 173, 223, 247);
     }
 
-
+    // posición de los enemigos y estrellas
     for (final estrella in estrellas!.objects) {
-      //print("DEBUG: ----->>>>  "+estrella.x.toString()+"    "+estrella.y.toString());
-      //EmberPlayer estrellaComponent = EmberPlayer(position: Vector2(estrella.x,estrella.y));
       StarElement estrellaComponent = StarElement(
           position: Vector2(estrella.x, estrella.y));
       objetosVisuales.add(estrellaComponent);
@@ -51,7 +55,6 @@ class UghGame extends FlameGame with HasKeyboardHandlerComponents,HasCollisionDe
     }
 
     for (final gota in gotas!.objects) {
-      //print("DEBUG: ----->>>>  "+estrella.x.toString()+"    "+estrella.y.toString());
       GotaPlayer gotaComponent = GotaPlayer(position: Vector2(gota.x, gota.y));
       objetosVisuales.add(gotaComponent);
       add(gotaComponent);
