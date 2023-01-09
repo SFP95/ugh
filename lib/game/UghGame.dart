80a9ab92416ce9917e225c38f6fbde82b0b22483
Nia <103027252+SFP95@users.noreply.github.com>
2023-01-09 13:45:48 +0100

    ajustes + arreglos main

--- lib/game/UghGame.dart
+++ lib/game/UghGame.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
@@ -14,8 +16,9 @@ class UghGame extends Forge2DGame with HasKeyboardHandlerComponents,HasCollision
   final double moveSpeed = 200;
   int starsCollected = 0;
   int health=3;
-  List<PositionComponent>  objetosvisuales=[];
   late EmberBody _emberBody;
+
+  List<PositionComponent>  objetosVisuales=[];
 
   UghGame():super(gravity: Vector2(0, 9.8),zoom: 0.75);;
 
@@ -32,25 +35,109 @@ class UghGame extends Forge2DGame with HasKeyboardHandlerComponents,HasCollision
       'water_enemy.png',
     ]);
 
-    TiledComponent mapComponent = await TiledComponent.load("mapa.tmx",Vector2(32,32));
+    mapComponent = await TiledComponent.load("mapa.tmx",Vector2(32,32));
     add(mapComponent);
 
+    @override
+    Color backgroundColor() {
+      return const Color.fromARGB(255, 173, 223, 247);
+    }
+
+    @override
+    void update(double dt) {
+      // TODO: implement update
+      //position.add(Vector2(10.0*horizontalDirection, 10.0*verticalDirection));
+      //velocity.x = horizontalDirection * moveSpeed;
+      //velocity.y = verticalDirection * moveSpeed;
+      //mapComponent.position -= velocity * dt;
+      //for(final objVisual in objetosVisuales){
+      //  objVisual.position -= velocity * dt;
+      //}
+
+      if (health <= 0) {
+        overlays.add('GameOver');
+      }
+
+      super.update(dt);
+    }
+
+    void setDirection(int horizontalDirection, int verticalDirection){
+      this.horizontalDirection=horizontalDirection;
+      this.verticalDirection=verticalDirection;
+    }
+
+    void initializeGame(bool loadHud) async{
+      // Assume that size.x < 3200
+      objetosVisuales.clear();
+      mapComponent.position=Vector2(0, 0);
+
+      ObjectGroup? estrellas=mapComponent.tileMap.getLayer<ObjectGroup>("estrellas");
+      ObjectGroup? gotas = mapComponent.tileMap.getLayer<ObjectGroup>("gotas");
+      ObjectGroup? posinitplayer = mapComponent.tileMap.getLayer<ObjectGroup>("posinitplayer");
+      ObjectGroup? suelos = mapComponent.tileMap.getLayer<ObjectGroup>("suelos");
+
+      for(final suelo in suelos!.objects){
+        SueloBody body=SueloBody(tiledBody: suelo);
+        add(body);
+
+      }
+
+      for(final estrella in estrellas!.objects){
+        //print("DEBUG: ----->>>>  "+estrella.x.toString()+"    "+estrella.y.toString());
+        //EmberPlayer estrellaComponent = EmberPlayer(position: Vector2(estrella.x,estrella.y));
+        StarElement estrellaComponent = StarElement(position: Vector2(estrella.x,estrella.y));
+        objetosVisuales.add(estrellaComponent);
+        add(estrellaComponent);
+      }
+
+      for(final gota in gotas!.objects){
+        //print("DEBUG: ----->>>>  "+estrella.x.toString()+"    "+estrella.y.toString());
+        GotaPlayer gotaComponent = GotaPlayer(position: Vector2(gota.x,gota.y));
+        objetosVisuales.add(gotaComponent);
+        add(gotaComponent);
+      }
+
+      _emberBody = EmberBody(position: Vector2(posinitplayer!.objects.first.x,posinitplayer!.objects.first.y));
+
+
+      add(_emberBody);
+
+
+
+      if (loadHud) {
+        add(Hud());
+      }
+    }
 
    ObjectGroup? estrellas = mapComponent.tileMap.getLayer<ObjectGroup>("strellas");
    ObjectGroup? gotas = mapComponent.tileMap.getLayer<ObjectGroup>("gotas");
 
-//esto da error y pondría las estrellas y gotas en el mata donde correpsonde
-
-   for (final estrella in estrellas!.objects){
-     StarElement estrellaComponent = StarElement(position: Vector2(estrella.x,estrella.y));
-      objetosvisuales.add(estrellaComponent);
-         }
-    for (final gota in gotas!.objects){
-      GotaPlayer gotaComponent = GotaPlayer(position: Vector2(gota.x,gota.y));
-      objetosvisuales.add(gotaComponent);
+    void reset() {
+      starsCollected = 0;
+      health = 3;
+      initializeGame(false);
     }
+    void joypadMoved(Direction direction){
+      //print("JOYPAD EN MOVIMIENTO:   ---->  "+direction.toString());
 
-    EmberPlayer emberPlayer = EmberPlayer(position: Vector2(300,300));
-    add(emberPlayer);
-  }
+      horizontalDirection=0;
+      verticalDirection=0;
+
+      if(direction==Direction.left){
+        horizontalDirection=-1;
+      }
+      else if(direction==Direction.right){
+        horizontalDirection=1;
+      }
+
+
+      if(direction==Direction.up){
+        verticalDirection=-1;
+      }
+      else if(direction==Direction.down){
+        verticalDirection=1;
+      }
+
+      _emberBody.horizontalDirection=horizontalDirection;
+    }
 }
