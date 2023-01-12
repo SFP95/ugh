0ec83f646b8c8bbd8d1b1750cfd344a5fbfe6325
alumno <dam.sonia.fernandez@gmail.com>
2023-01-12 20:02:33 +0100

    añadir menu al main mas game over + creacion de joypad

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:ugh/ux/joypad.dart';
+import 'package:ugh/widgets/GameOver.dart';
 
+import 'game/MainMenu.dart';
 import 'game/UghGame.dart';
 
 void main() {
 
   runApp(
-    const GameWidget<UghGame>.controlled(
+     GameWidget<UghGame>.controlled(
       gameFactory: UghGame.new,
+      overlayBuilderMap: {
+        'MainMenu': (_, game) => MainMenu(game: game),
+        'GameOver': (_, game) => GameOver(game: game),
+        //'Joypad': (_, game) => Joypad(onDirectionChanged: game.joypadMoved),
+      },
+      initialActiveOverlays: const ['MainMenu'],
     ),
   );
-  /** //otra forma de hacerlo
-   * final game = UghGame();
-      runApp(GameWidget(game: game));
-    */
-
 }
